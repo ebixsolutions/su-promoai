0fee1cebe567c89693d41fb4c04d466901c9cce4
cms <64578119+ebixsolutions@users.noreply.github.com>
2026-05-28 14:28:51 +0800

    update-config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/su-promoai/',
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
     base44({
